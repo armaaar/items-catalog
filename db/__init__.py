484731cb193464b4f11dd5aa92545532406dc117
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2017-06-27 14:21:45 +0200

    Create db package to interface the db easily

---
+++ db/__init__.py
@@ -0,0 +1,22 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from _base import Base
+
+from table import Sample
+
+engine = create_engine('sqlite:///test.db')
+Base.metadata.create_all(engine, checkfirst=True)
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+db_session = DBSession()
+
+__all__ = ["db_session", "Sample"]

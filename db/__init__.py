8005d0c257b1bdd832b302f706015a61c68d4504
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2017-06-27 15:04:08 +0200

    Change db name

--- db/__init__.py
+++ db/__init__.py
@@ -7,7 +7,7 @@ from user import User
 from item import Item
 
 
-engine = create_engine('sqlite:///test.db')
+engine = create_engine('sqlite:///catalog.db')
 Base.metadata.create_all(engine, checkfirst=True)
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
